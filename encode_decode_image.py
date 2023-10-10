87868b75d4ded282f4b45ec83a188e3b487ca913
ilham-2001 <arizqyakbar@gmail.com>
2023-10-11 04:29:26 +0700

    test on sub image then pub pos

--- encode_decode_image.py
+++ encode_decode_image.py
@@ -1,5 +1,6 @@
 import base64
 import cv2 as cv
+import numpy as np
 
 def encode_image_to_base64(im):
     try:
@@ -13,6 +14,22 @@ def encode_image_to_base64(im):
         base64_encoded_image_str = base64_encoded_image.decode('utf-8')
 
         return base64_encoded_image_str
+    except Exception as e:
+        print("Error:", str(e))
+        return None
+    
+def decode_base64_to_image(base64_encoded_image_str):
+    try:
+        # Decode the base64 string to bytes
+        img_data = base64.b64decode(base64_encoded_image_str)
+        
+        # Convert bytes to numpy array
+        nparr = np.frombuffer(img_data, np.uint8)
+        
+        # Decode numpy array to image using OpenCV
+        img = cv.imdecode(nparr, cv.IMREAD_COLOR)
+        
+        return img
     except Exception as e:
         print("Error:", str(e))
         return None
--- test_sub.py
+++ test_sub.py
@@ -1,25 +1,24 @@
-import cv2
-import base64
-import numpy as np
 import paho.mqtt.client as mqtt
 import cv2 as cv
+import numpy as np
 from encode_decode_image import decode_base64_to_image
 
 
 def on_connect(client, userdata, flags, rc):
     print("[CONNECTED]")
 
-
 def on_message(client, userdata, msg):
     if msg.topic == "/labsyms/image":
         b64_im = msg.payload.decode().split(",")[1]
         im = decode_base64_to_image(b64_im)
-        # print(im)
-        cv.imshow("Frame", im)
+        
+        cv.imshow("frame", im)
         cv.waitKey(1)
 
 
 if __name__ == "__main__":
+    # print(encode_decode_image.decode_base64_to_image([1, 2, 3, 4]))
+    # decode_base64_to_image([[1, 2, 3, 4]])
     mqttc = mqtt.Client(transport="websockets")
     mqttc.connect("127.0.0.1", 9001)
 
@@ -27,5 +26,4 @@ if __name__ == "__main__":
     mqttc.on_message = on_message
 
     mqttc.subscribe("/labsyms/image")
-
     mqttc.loop_forever()
